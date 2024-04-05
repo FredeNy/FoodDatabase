9159ef904a8d0c20359479ad35d49ff29ef301b7
Frederikke <frny0001@stud.kea.dk>
2024-04-05 08:49:54 +0200

    foodbase

---
+++ modules/settings.js
@@ -0,0 +1,5 @@
+
+export const apikey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0cWd2aHR4eWxzZHJ1ZnB1amJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NTE3NjAsImV4cCI6MjAyNjMyNzc2MH0.tEB8VM6MpXaq0geRZ7vHsyaHW5Hl6UYPqumGtq1a-JE";
+export const endpoint =
+  "https://ttqgvhtxylsdrufpujbw.supabase.co/rest/v1/FoodBase";
---
+++ modules/setup.js
@@ -0,0 +1,13 @@
+
+import { countries } from "./countries.js";
+
+export async function setupCountries() {
+  const list = document.querySelector("#countrylist");
+  const frgmt = document.createDocumentFragment();
+  countries.forEach((c) => {
+    const option = document.createElement("option");
+    option.value = c.name;
+    frgmt.appendChild(option);
+  });
+  list.appendChild(frgmt);
+}
